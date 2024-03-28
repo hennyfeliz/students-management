a2cb300c20fe4bf5ad4b2b54c396644d1e6c5a96
Henny Feliz <mingito.2017@gmail.com>
2024-03-28 00:29:04 -0400

    School entity class

---
+++ back/src/main/java/org/acme/entities/School.java
@@ -0,0 +1,47 @@
+package org.acme.entities;
+
+import com.fasterxml.jackson.annotation.*;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import io.quarkus.hibernate.orm.panache.PanacheQuery;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "School")
+@Cacheable
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter
+@EqualsAndHashCode(callSuper = false)
+public class School extends PanacheEntityBase {
+
+    @Id
+    @Column(name = "school_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    @Column(name = "school_name", length = 100, nullable = false)
+    public String schoolName;
+
+    @Column(length = 255)
+    public String address;
+
+    @Column(name = "phone_number", length = 20)
+    public String phoneNumber;
+
+
+    @OneToMany(mappedBy = "school", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonIgnoreProperties("school")
+    @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+    public List<Teacher> teachers;
+
+    @OneToMany(mappedBy = "school", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonIgnoreProperties("school")
+    @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+    public List<Student> students;
+
+
+
+}
