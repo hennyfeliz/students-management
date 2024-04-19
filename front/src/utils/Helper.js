1a69f2a24cd75c698f876aeec85892ae57d8484a
Henny Feliz <mingito.2017@gmail.com>
2024-04-19 16:58:32 -0400

    Sidebar items helper array

---
+++ front/src/utils/Helper.js
@@ -0,0 +1,32 @@
+export const SidebarItems = [
+  {
+    link: "",
+    text: "Schools",
+    icon: "bx bx-store-alt",
+  },
+  {
+    link: "",
+    text: "Students",
+    icon: "bx bx-group",
+  },
+  {
+    link: "",
+    text: "Analytics",
+    icon: "bx bx-analyse",
+  },
+  {
+    link: "",
+    text: "Tickets",
+    icon: "bx bx-message-square-dots",
+  },
+  {
+    link: "",
+    text: "Users",
+    icon: "bx bx-group",
+  },
+  {
+    link: "",
+    text: "Settings",
+    icon: "bx bx-cog",
+  },
+];
