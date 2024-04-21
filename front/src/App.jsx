eee6e0194a9e165a82b05a25f004458c59feb592
Henny Feliz <mingito.2017@gmail.com>
2024-04-21 15:24:36 -0400

    App js component

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,34 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import { Sidebar } from './components/Sidebar';
+import { MainTwo } from './components/MainTwo';
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [sidebar, setSidebar] = useState(false);
+  const [datatableIndex, setDatatableIndex] = useState(0);
+
+  const datatableIndexHandler = (index) => {
+    setDatatableIndex(index)
+  }
+
+  const sidebarHandler = () => {
+    setSidebar(!sidebar)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    // <Datatable />
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path='/' element={<Login />} /> */}
+        <Route path='/users/*' element={
+          <>
+            {
+              /*
+              <Header />
+              */
+            }
+            <Sidebar datatableIndexHandler={datatableIndexHandler} sidebar={sidebar} />
+            <MainTwo datatableIndex={datatableIndex} sidebarHandler={sidebarHandler} />
+            {
+              /*
+              <div className='principal-content'>
+                <Main />
+              </div>
+              */
+            }
+          </>
+        } />
+      </Routes>
+
+    </BrowserRouter>
   )
 }
 
