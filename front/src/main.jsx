b907737630e0dcbbab094f3fb6ba47f268f65bdc
Henny Feliz <mingito.2017@gmail.com>
2024-04-23 00:05:32 -0400

    main component changes

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -2,9 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { PrimeReactProvider } from 'primereact/api';
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css'
+import 'primeicons/primeicons.css'
+
+// ROBOTO FONT FOR MATERIAL UI
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <PrimeReactProvider>
+      <App />
+    </PrimeReactProvider>
   </React.StrictMode>,
 )
