3fc53f34ea17abf1013b8b77d60c928b0e9717c0
Henny Feliz <mingito.2017@gmail.com>
2024-05-18 00:15:52 -0400

    fetch js file changes and table css

--- front/src/fetch.js
+++ front/src/fetch.js
@@ -3,22 +3,26 @@ export const getData = ({
   index = 1,
   size = 10,
   sort = "",
+  filters = {},
   method = "GET",
   headers = { "Content-Type": "application/json" },
 }) => {
-  const url = `http://localhost:8080/${path}?page=${
-    index - 1
-  }&size=${size}&sort=${sort}`;
+  const queryParams = new URLSearchParams({
+    page: index - 1,
+    size,
+    sort,
+    ...filters,
+  });
+
+  const url = `http://localhost:8080/${path}?${queryParams.toString()}`;
 
   return fetch(url, {
     method: method,
     headers: headers,
   })
-    .then((response) => {
-      return response.json();
-    })
+    .then((response) => response.json())
     .catch((error) => {
-      console.error("Error al intentar obtener los resultados: ", error);
+      console.error("Error fetching data: ", error);
       throw error;
     });
 };
