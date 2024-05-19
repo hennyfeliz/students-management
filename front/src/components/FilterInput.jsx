26aa5c578fb0804c6727e6b87c9b3456f9404cea
Henny Feliz <mingito.2017@gmail.com>
2024-05-19 00:51:01 -0400

    Filter input component changes, adding correct properties

--- front/src/components/FilterInput.jsx
+++ front/src/components/FilterInput.jsx
@@ -1,10 +1,19 @@
+/* eslint-disable react/prop-types */
 import { MagnifierIcon } from "../assets/icons/Icons"
 
-export const FilterInput = () => {
+export const FilterInput = ({ filterKey, onFilterChange }) => {
+  const handleChange = (event) => {
+    onFilterChange(filterKey, event.target.value);
+  };
+
   return (
-    <div className='keyword-input-text input-short'>
+    <div className="keyword-input-text input-short">
       <MagnifierIcon />
-      <input type="text" placeholder='Keyword Search' />
+      <input
+        type="text"
+        placeholder="Keyword Search"
+        onChange={handleChange}
+      />
     </div>
-  )
-}
+  );
+};
