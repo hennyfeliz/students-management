4724152489bc66bdc80aa48653ca59449dc972c1
Henny Feliz <mingito.2017@gmail.com>
2024-05-20 00:50:51 -0400

    Teacher entity, adding some new query methods for filtering and named query annonations

--- back/src/main/java/org/acme/entities/Teacher.java
+++ back/src/main/java/org/acme/entities/Teacher.java
@@ -2,8 +2,10 @@ package org.acme.entities;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.quarkus.hibernate.orm.panache.PanacheQuery;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import io.quarkus.panache.common.Parameters;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.OnDelete;
@@ -19,6 +21,14 @@ import java.util.List;
 @Getter
 @Setter
 @EqualsAndHashCode(callSuper = false)
+@NamedQueries({
+        @NamedQuery(name = "Teacher.findByTeacherName",
+                query = "FROM Teacher WHERE LOWER(teacherName) LIKE LOWER(CONCAT('%', :teacherName, '%'))"),
+        @NamedQuery(name = "Teacher.findBySubjectTaught",
+                query = "FROM Teacher WHERE LOWER(subjectTaught) LIKE LOWER(CONCAT('%', :subjectTaught, '%'))"),
+        @NamedQuery(name = "Teacher.findBySchool",
+                query = "FROM Teacher WHERE school.id = :schoolId")
+})
 public class Teacher extends PanacheEntityBase {
 
     @Id
@@ -43,5 +53,17 @@ public class Teacher extends PanacheEntityBase {
     @JsonIgnoreProperties("teacher")
     @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
     public List<Class> classes;
+
+    public static PanacheQuery<Teacher> findByTeacherName(String teacherName) {
+        return find("#Teacher.findByTeacherName", Parameters.with("teacherName", teacherName));
+    }
+
+    public static PanacheQuery<Teacher> findBySubjectTaught(String subjectTaught) {
+        return find("#Teacher.findBySubjectTaught", Parameters.with("subjectTaught", subjectTaught));
+    }
+
+    public static PanacheQuery<Teacher> findBySchool(Long schoolId) {
+        return find("#Teacher.findBySchool", Parameters.with("schoolId", schoolId));
+    }
 
 }
