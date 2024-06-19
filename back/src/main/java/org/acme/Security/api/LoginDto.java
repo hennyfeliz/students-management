d385af7d66425ab6c892bd028247abe8c3005f5a
Henny Feliz <mingito.2017@gmail.com>
2024-06-19 00:20:32 -0400

    LoginDtop and Message records for auth endpoints responses and requests

---
+++ back/src/main/java/org/acme/Security/api/LoginDto.java
@@ -0,0 +1,14 @@
+package org.acme.Security.api;
+
+import jakarta.validation.constraints.NotNull;
+
+public record LoginDto(@NotNull String username, @NotNull String password) {
+
+    @Override
+    public String toString() {
+        return "LoginDto{" +
+                "username='" + username + '\'' +
+                ", password='*********'" +
+                '}';
+    }
+}
